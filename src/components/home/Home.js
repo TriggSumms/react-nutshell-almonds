35693c6fe2a3d374ef23503988a3a9f43e42aa9d
jonrrile <jonrrile@ut.utm.edu>
2020-07-21 13:12:38 -0500

    css stylings and profile pics

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,20 +7,16 @@ const Home = props => {
   return (
     <>
 
-      <div>
+      
       
 
         <div className="friend__Container">
         </div>
-      </div>
 
-
-      <div>
         {/* <h2>Chat/Messaging Windows </h2> */}
         <div className="chat___Container"></div>
         <section className="constructedMessage__Container"></section>
       {/* <button onClick={refreshPageAfterMessage}>Reload &#x27f3;</button> */}
-      </div>
     </>
   )
 };
